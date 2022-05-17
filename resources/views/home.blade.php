63fca6039ecb1cf7571705d082d05182b1e082d0
Reynxzz <luthfirey258@gmail.com>
2022-05-17 11:06:08 +0700

    pindahin css, +env, composer install

---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xerabutan - Salurkan Keahlianmu!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24">
+          </a>
+          <div class="d-flex">
+              <div class="menu mt-2">
+                <a href="#" class="text-decoration-none biru-xerabutan fw-bold">Home</a>
+                <a href="#" class="ms-3 text-decoration-none biru-xerabutan fw-bold">Cari Keahlian</a>
+                <a href="#" class="ms-3 text-decoration-none biru-xerabutan fw-bold">Tentang Kami</a>  
+              </div>
+              <div class="btn bg-biru-xerabutan text-wrap ms-3 me-5" style="width: 5em;">
+                <a href="#" class="text-decoration-none text-white fw-bold fs-6 btn-login">Login</a>
+              </div>
+          </div>
+        </div>
+    </nav>
+
+    <footer class="bg-biru-xerabutan">
+        <div class="container py-4">
+            <div class="row text-center text-white">
+                <div class="col text-start">
+                    <p>
+                        contact@xerabutan.com <br>
+                        0813-9868-9316<br>
+                        Jalan Raya Cibanteng, Kabupaten Bogor.<br>
+                        @xerabutan<br>
+                    </p>
+                </div>
+                <div class="col">
+                    <p class="fw-bold fs-1">#Salurkan Keahlianmu</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
