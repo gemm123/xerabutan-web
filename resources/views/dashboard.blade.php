cbee055cf5d5c82984f469d7ebe01b8c3fa7fc12
gemm123 <gemaakbar213@gmail.com>
2022-05-25 14:39:42 +0700

    add dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,13 @@
-<h1>Selamat datang, {{ auth()->user()->name }}</h1>
+@extends('layouts.dashboard')
+@section('template-dashboard')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Selamat datang, {{ auth()->user()->name }}</h1>
+        </div>
+    </div>
+</div>
+@endsection
+    
+
+    
