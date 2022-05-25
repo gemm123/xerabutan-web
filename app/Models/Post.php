6dda41a4b8f5a42ce5b46968ed51c1eb4cf55c89
Reynxzz <luthfirey258@gmail.com>
2022-05-25 21:30:10 +0700

    post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+//use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+    //Sluggable;
+
 
     protected $guarded = ['id'];
 
@@ -51,4 +54,19 @@ class Post extends Model
     public function getRouteKeyName(){
         return 'slug';
     }
+
+        /**
+         * Return the sluggable configuration array for this model.
+         *
+         * @return array
+         */
+        
+    // public function sluggable(): array
+    // {
+    //     return [
+    //         'slug' => [
+    //             'source' => 'title'
+    //         ]
+    //     ];
+    // }
 }
