5eafa0b8ff5e6373024061b7e64f3bdc6842c973
Reynxzz <luthfirey258@gmail.com>
2022-05-25 17:07:52 +0700

    benerin active

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ms-auto">
               <li class="nav-item">
-                <a class="nav-link  biru-xerabutan fw-bold menu {{ Request::is('home') ? 'active' : '' }}" href="/">Home</a>
+                <a class="nav-link  biru-xerabutan fw-bold menu {{ Request::is('/') ? 'active' : '' }}" href="/">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link biru-xerabutan fw-bold menu {{ Request::is('posts') ? 'active' : '' }}" href="/posts">Cari Keahlian</a>
